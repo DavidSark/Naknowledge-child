bed4e277f67f9abc29035347fdc41c39e4d2156a
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-01-10 23:54:23 +0100

    mise en forme de la page inscription

---
+++ page-register.php
@@ -0,0 +1,19 @@
+
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+
+<body class="body_login">
+
+<!--image de fond des les exagones en haut a droite-->
+
+
+<h1 class="title_login">Rejoignez Naknowledge !</h1>
+
+<div class="form_login">
+    <?php the_content()?>
+</div>
+
+<!--image de fond des les exagones en bas a gauche -->
+
+
+</body>
+
