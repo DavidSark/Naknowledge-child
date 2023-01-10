f1f634dbbbed9659baf14fd26d3d01cf1cca2b43
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-01-10 22:20:41 +0100

    mise en forme de la page 404

--- 404.php
+++ 404.php
@@ -1,10 +1,23 @@
-<?php get_header()?>
-   
-  
-    <div class="">
-        <h1 class="titre_article">Page introuvable</h1>
-        <p>Cette page n'existe pas</p>
-    </div>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
 
-<?php get_footer()?>
+<body class="body_404">
+    <div class="">
+        <div class="bloc_404">
+            <p>4</p>
+            <img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/logo_naknow.svg" alt="logo naknowledge">
+            <p>4</p>
+        </div>  
+        <p class="introuvable">Cette page est introuvable !</p>
+        
+        <p class="retour">
+            <a  href="http://localhost/naknowledge/naknowlecons/"> Retourner aux leçons.. </a>
+        </p>
+
+        <div class="bloc_centre_img_404">
+            <img class="img_404" src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/img_404.svg" alt="image 404">
+        </div>
+    </div>
+    
+</body>
+
 
