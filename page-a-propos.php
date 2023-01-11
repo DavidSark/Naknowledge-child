b0b7014649b698c5f3dac9b50ff9815940f6ef98
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-01-11 02:56:40 +0100

    page à propos

---
+++ page-a-propos.php
@@ -0,0 +1,57 @@
+
+<div class="top_espace2"></div>
+
+<?php get_header()?>
+
+<div class="bloc_contenu_à_propos">
+    <h2 class="titre_à_propos">Qu'est ce que Naknowledge ?</h2>
+    <p>Naknowledge, une plateforme qui regroupe des utilisateurs pour partager leurs connaissances.</p>
+    <p>Nous offrons donc la possibilité à l’utilisateur de partager ce qu’il sait faire dans n’importe quel domaine, jeux vidéos, développement, 
+    vous choisissez !</p>
+    
+
+    <h2 class="titre_qui_somme_nous">Qui sommes nous ?</h2>
+    <div class="bloc_contenu_avatar">
+        <div class="avatar">
+            <img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/avatar_melina.svg" alt="avatar concepteur">
+            <div class="description_equipe">
+                <p>Mélina Bouchelleghem </p> 
+                <p>Responsable de CDP & développeuse web</p>
+            </div>
+        </div>
+
+        <div class="avatar">
+            <img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/avatar_david.svg" alt="avatar concepteur">
+            <div class="description_equipe">
+                <p>David Sarkissian</p> 
+                <p>Chargé de com’ & développeur web</p>
+            </div>
+        </div>
+
+        <div class="avatar">
+            <img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/avatar_aurelie.svg" alt="avatar concepteur">
+            <div class="description_equipe">
+                <p>Aurélie Runser</p> 
+                <p>Chargé de com’ & développeuse web</p>
+            </div>
+        </div>
+
+        <div class="avatar">
+            <img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/avatar_rose.svg" alt="avatar concepteur">
+            <div class="description_equipe">
+                <p>Rose Menegain</p> 
+                <p>Responsable de design</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="bloc_txt_explicatif">
+        <p class="txt_explicatif">Notre équipe ce compose de trois étudiant en deuxième année de BUT MMI. 
+        Notre projet est donc de créé Naknowledge, l’idée venant de 
+        Pascal Chatonnay.</p>
+    </div>
+</div>
+
+<div class="bot_espace"></div>
+
+<?php get_footer()?>
