ccb5df24da03885e72a03a6e7ea1abb6765cbfb2
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-01-11 01:20:50 +0100

    politique de confidentialité

---
+++ page-politique-de-confidentialite.php
@@ -0,0 +1,13 @@
+
+<div class="top_espace2"></div>
+
+<?php get_header()?>
+
+<div class="bloc_contenu">
+<p class="titre_politique">Politique de Confidentialité</p>
+    <?php the_content()?>
+</div>
+
+<div class="bot_espace"></div>
+
+<?php get_footer()?>
