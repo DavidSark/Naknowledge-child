1730e06574c96bcd61d7fc983d7c2600a8750949
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-01-14 03:42:31 +0100

    changement de style du lien sur les card vers les lecons

--- parts/post.php
+++ parts/post.php
@@ -1,25 +1,24 @@
 <div class="card">
 
 <!-- Appel de l'image en tant que miniature -->
-           <div class="card-bloc_image">
-               <?php the_post_thumbnail('medium')?>
-           </div>
+    <div>
+         
+        <div class="card-bloc_image">
+            <?php the_post_thumbnail('medium')?>
+        </div>
 
-               <figure class="card-user_info">
-                   <!-- Appel de l'image de profil et du nom de l'auteur de l'article-->
-                   <fig class="user_image">
-                       <?php echo get_avatar( get_the_author_email(), 35 );?>
-                   </fig>
-                   <figcaption class="user_name">
-                       <?php the_author()?>
-                   </figcaption>
-                </figure>
+        <figure class="card-user_info">
+            <!-- Appel de l'image de profil et du nom de l'auteur de l'article-->
+            <fig class="user_image">
+                <?php echo get_avatar( get_the_author_email(), 35 );?>
+            </fig>
+            <figcaption class="user_name">
+                <?php the_author()?>
+            </figcaption>
+        </figure>
 
-                <!-- <div class="card-favorie">
-                    <?php wpfp_link() ?>
-                </div> -->
 
-               <div class="card-niveau">
+        <div class="card-niveau">
                    <?php 
                        $category = get_field('niveaux_de_difficulte');
                        if (($category == 1) || ($category == 0)) {
@@ -51,36 +50,35 @@
                        }
                    ?>
 
-               </div>
+        </div>
 
            
-               <!--Appel des étiquettes (tags) de l'article -->
-               <p class="card-tags">
-                   <?php 
-                       $post_tags = wp_get_post_tags(get_the_ID());
-                       foreach ($post_tags as $tag){
-                       echo '<a href="'.get_tag_link($tag->term_id).'">#'.$tag->name.'</a> ';
-                       }
-                       ?>
-               </p>
+        <!--Appel des étiquettes (tags) de l'article -->
+        <p class="card-tags">
+            <?php 
+                $post_tags = wp_get_post_tags(get_the_ID());
+                foreach ($post_tags as $tag){
+                    echo '<a href="'.get_tag_link($tag->term_id).'">#'.$tag->name.'</a> ';
+                }
+            ?>
+        </p>
 
                
-               <!-- Appel du titre de l'article-->         
-               <h2 class="card-titre">    
-                   <?php the_title() ?>
-                    
-               </h2>
+        <!-- Appel du titre de l'article-->         
+        <h2 class="card-titre">    
+            <?php the_title() ?>
+        </h2>
                
-               <!--Appel de la description de l'article  -->
-               <p class="card-text">
-                   <?php 
-                       $value = get_field('description_lecon', $post->ID); 
-                       echo " $value "
-                   ?>
-               </p>
+        <!--Appel de la description de l'article  -->
+        <p class="card-text">
+            <?php 
+                $value = get_field('description_lecon', $post->ID); 
+                 echo " $value "
+            ?>
+        </p>
+    </div>
 
-
-               <!--Appel du lien permettant d'aller à l'article -->
-               <p class="card-lien"><a href="<?php the_permalink() ?>">Lire la leçon</a></p>
-           
-           </div>
+    <!--Appel du lien permettant d'aller à l'article -->
+    <p class="card-lien"><a href="<?php the_permalink() ?>">Lire la naknowleçon</a></p>
+          
+</div>
