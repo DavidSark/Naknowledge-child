6d5adf99264deea1e5eb871457ae75820e524ee9
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-01-14 02:13:20 +0100

    mise en forme de la page single-post.php

--- single-post.php
+++ single-post.php
@@ -1,110 +1,139 @@
 <?php get_header()?>
 <div class="top_espace"></div>
 
+<div class="header_post">
+    <button class="button_back" onclick="history.go(-1);"><img class="button_back-img" src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/icone_retour.png" alt="Bouton retour en arrière"></button>
+</div>
 
-<?php if (have_posts()): ?>
-   
-    <?php while(have_posts()): the_post();?>
 
-    <div class="">
+<div class="single_post">
+    <div class="lecon">
 
-        <div class="card-bloc_image">
+        <div class="lecon_bloc1">
+            <div class="card-bloc_image lecon_image">
+                <?php 
+                    $image_id = get_field('image_mise_en_avant', $post->ID);
+                    set_post_thumbnail( $post->ID, $image_id );
+                ?>  
                 <?php the_post_thumbnail('medium' )?>   
-        </div>
-
-        <figure class="card-user_info">
+            </div>
+        
+            <figure class="card-user_info lecon-user_info">
                 <!-- Appel de l'image de profil et du nom de l'auteur de l'article-->
-                <fig class="user_image">
+                <fig class="user_image lecon-user_image">
                     <?php echo get_avatar( get_the_author_email(), 35 );?>
-                 </fig>
-           <figcaption class="user_name">
-                      <?php the_author()?>
-            </figcaption>
-        </figure>
-
-        <div class="card-niveau">
-                    <?php 
-                        $category = get_field('niveau');
+                </fig>
+                <figcaption class="user_name">
+                    <?php the_author()?>
+                </figcaption>
+            </figure>
+    
+            <div class="lecon-info2">
+                <div class="card-niveau lecon-niveau">
+                    <?php $category = get_field('niveau');
                         if (($category == 1) || ($category == 0)) {
                             echo '<img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/niveau_1.svg"/>';
                         }
                     ?>
-                    <?php 
-                        $category = get_field('niveau');
+                    <?php $category = get_field('niveau');
                         if ($category == 2) {
                             echo '<img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/niveau_2.svg"/>';
                         }
                     ?>
-                    <?php 
-                        $category = get_field('niveau');
+                    <?php $category = get_field('niveau');
                         if ($category == 3) {
                             echo '<img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/niveau_3.svg"/>';
                         }
                     ?>
-                    <?php 
-                        $category = get_field('niveau');
+                    <?php $category = get_field('niveau');
                         if ($category == 4) {
                             echo '<img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/niveau_4.svg"/>';
                         }
                     ?>
-                    <?php 
-                        $category = get_field('niveau');
+                    <?php $category = get_field('niveau');
                         if ($category == 5) {
                             echo '<img src="http://localhost/naknowledge/wp-content/themes/underscore-child/images/icones/niveau_5.svg"/>';
                         }
                     ?>
-
+            
                 </div>
-
-                <p class="card-tags">
+        
+                <div class="lecon-favorie">
+                    <?php wpfp_link() ?>
+                </div>
+            </div>
+        
+            <div class="lecon-infos">
+                <!-- Appel de la catégorie de l'article -->
+                <p class="lecon-categorie">
                     <?php 
-                        $post_tags = wp_get_post_tags(get_the_ID());
-                        foreach ($post_tags as $tag){
-                        echo '<a href="'.get_tag_link($tag->term_id).'">#'.$tag->name.'</a> ';
+                        $categories = get_the_category();
+                        foreach ( $categories as $category ) {
+                            echo '<a href="' . get_category_link( $category->term_id ) . '"> ' . $category->name . '</a> ';
                         }
-                        ?>
+                    ?>
+                </p> 
+            
+                <p class="card-tags lecon-tags">
+                    <?php $post_tags = wp_get_post_tags(get_the_ID());
+                        foreach ($post_tags as $tag){
+                            echo '<a href="'.get_tag_link($tag->term_id).'">#'.$tag->name.'</a> ';
+                        }
+                    ?>
                 </p>
+            </div>
+        </div>
+    
+            
+        <div class="lecon_bloc1">
+            <h1 class="lecon-titre"><?php the_title() ?></h1>
+            <div class="lecon-text"><?php the_field('contenu_lecon') ?></div>
+        </div>
 
+    </div>
+    
+    <div class="autres_lecon">
+    
+        <?php
+            $terms = get_the_terms( $post->ID, array( 'category', 'post_tag' ) );
+            $term_ids = wp_list_pluck( $terms, 'term_id' );
 
-                <h1 class="titre_article">Test<?php the_title()?></h1>
-        
- 
-          <?php the_post_thumbnail('card-image'); ?> 
-        <?php the_content()?>
+            $related_query = new WP_Query( array(
+                'post__not_in' => array( $post->ID ),
+                'tax_query' => array(
+                    'relation' => 'OR',
+                    array(
+                        'taxonomy' => 'category',
+                        'field' => 'id',
+                        'terms' => $term_ids
+                    ),
+                    array(
+                        'taxonomy' => 'post_tag',
+                        'field' => 'id',
+                        'terms' => $term_ids
+                    )
+                )
+            ) );
 
+            if ( $related_query->have_posts() ) : ?>
 
+            <h2 class="autres_lecon-titre">Naknowleçons en rapport</h2>
 
+            <div class="lecons_grille">
+                <?php while ( $related_query->have_posts() ) : $related_query->the_post(); ?>
+                            
+                <?php get_template_part('parts/post', 'post'); ?>
 
-        
-        <?php endwhile; endif ?>
+                <?php endwhile; ?>
+            </div>
+
+            <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
+
+    
     </div>
 
-
-
-<h2>Leçons liées</h2>
-
-<?php 
-$jeux = array_map(function($term){
-    return $term->term_id;
-}, get_the_terms(get_post(), 'jeux_video'));
-$query = new WP_Query([
-    'post__not_in' => [get_the_ID()],
-    'post_type' => 'post',
-    'posts_per_page' => 3,
-    'orderby' => 'rand',
-    'tax_query' => [[
-        'taxonomy' => 'jeux_video',
-        'terms' => $jeux,
-
-    ]]
-]);
-while($query->have_posts()): $query->the_post();
-?>
-        <div>
-            <?php get_template_part('parts/post', 'post'); ?>
-        </div>
-<?php endwhile; wp_reset_postdata() ?>
-
+</div>
 
 
 <?php get_footer()?>
