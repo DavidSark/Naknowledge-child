1b2e43331223c455eaf7d16cdb74671bae61c165
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-01-15 11:33:05 +0100

    ajout et mise en forme de la page register

--- page-register.php
+++ page-register.php
@@ -1,4 +1,4 @@
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
 
 <body class="body_login">
