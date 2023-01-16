6d86dced23f66e99fe46816eeeeb0e462dba2664
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2023-01-16 14:52:33 +0100

    MAJ barre WP

--- functions.php
+++ functions.php
@@ -130,4 +130,20 @@ function fav_shortcode() {
  add_shortcode( '[wp-favorite-posts]', 'fav_shortcode' );
  
  
- add_filter('show_admin_bar', '__return_false');
+//Code pour ne pas montrer la barre de navigation admin wordpress, seulement 
+//pour les admins
+if (!current_user_can('manage_options')) {
+	
+    remove_action( 'admin_footer', 'wp_admin_bar_render', 1000 );
+    remove_action('wp_footer', 'wp_admin_bar_render', 1000);
+
+    function remove_admin_bar_style() {  
+  	echo '<style>body.admin-bar #wpcontent, body.admin-bar #adminmenu { padding-top: 0px !important; }</style>';	
+    }
+    add_filter('admin_head','remove_admin_bar_style');
+
+    function remove_admin_bar_style_frontend() {
+        echo '<style>html{ padding-top: 0px !important; }</style>';
+    }
+    add_filter('wp_head','remove_admin_bar_style_frontend');
+}
